a779391106bcea450e69fd9bd18fb3a535dcaa8d
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 11:23:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// write code here
+function shoppingCart() {
+  let cart = {
+    productName: 0,
+    number: 0,
+    pricePerUnit: 0,
+    discountPercent: 0
+  }
+  for (let key in cart) {
+    let value = prompt(`${key}?`)
+    if (key === 'productName') {
+      cart[key] = value
+    } else {
+      cart[key] = Number(value)
+    }
+  }
+  if (cart.discountPercent === 0) {
+    delete cart.discountPercent
+  }
+  console.log(cart)
+
+function calculateDiscountedPrice(pricePerUnit, discountPercent) {
+  return pricePerUnit - (pricePerUnit * (discountPercent / 100))
+}
+  if ('discountPercent' in cart) {
+    console.log(`Discounted Price: ${calculateDiscountedPrice(cart.pricePerUnit, cart.discountPercent)}`)
+  }
+}
+
